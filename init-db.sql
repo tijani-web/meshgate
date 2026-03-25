3b1201b105f8a0d5067a615dee2e30324e79e74d
Tijani Basit <tijanibwebdev@gmail.com>
2026-03-25 12:53:52 -0700

    chore: align connectivity and configuration for production deployment

---
+++ init-db.sql
@@ -0,0 +1,5 @@
+-- Create missing logical databases for each microservice
+CREATE DATABASE eureka_auth_db;
+CREATE DATABASE eureka_billing_db;
+CREATE DATABASE eureka_notification_db;
+CREATE DATABASE eureka_user_db;
